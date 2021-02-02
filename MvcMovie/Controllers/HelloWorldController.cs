80244fc46f7a24672c812cb06a6371a98860196e
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-02 02:12:48 +0200

    I created the web app and I implemented HelloWorldController and Index view.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Get: /HelloWorld/Welcome/
+        public string Welcome(string name, int ID = 1)
+        {
+            // Uses HtmlEncoder.Default.Encode to protect the app from malicious input (namely JavaScript)
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
