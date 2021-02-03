bf868a9caabdca691b8c6638dad3a0a0fb06ad9e
racoltacalin <racolta.cvasile@hotmail.com>
2021-02-03 15:44:27 +0200

    Last push with updates about Validation

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -12,26 +12,19 @@ namespace MvcMovie.Models
         public int Id { get; set; }
 
         [StringLength(60, MinimumLength = 3)]
-        [Required]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
-        [Column(TypeName = "decimal(18,2)")] //https://mattferderer.com/entity-framework-no-type-was-specified-for-the-decimal-column
-        public decimal Price { get; set; }
-
-        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-        [Required]
-        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
+        [Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]             //https://mattferderer.com/entity-framework-no-type-was-specified-for-the-decimal-column
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; }
     }
 }
